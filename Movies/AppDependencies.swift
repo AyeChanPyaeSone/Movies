3c7c7a39b708dbc38e4c378f36d2ad07cabc4d2b
ayec <ayechanpyaesone.ac@gmail.com>
2026-04-25 14:35:09 +1000

    - move TMDB package under Movies project - add barer token

--- Movies/AppDependencies.swift
+++ Movies/AppDependencies.swift
@@ -3,7 +3,7 @@ import TMDBKit
 enum AppDependencies {
     static let movieClient = TMDBClient(
         configuration: TMDBConfiguration(
-            authorization: .placeholder
+            authorization: .bearerToken("eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YmUyNmVmYzU2NTRiM2NkYmM3NmIzMTg5YmE2OTA3OSIsIm5iZiI6MTc3NzA3NzQ4Mi45NCwic3ViIjoiNjllYzBjZWEyMDVjMThmYzg0OTM1OWI1Iiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.aZ29MtPvGG7uWehqZ9BVYmYzx7zLZsDonbd7EsQvoXE")
         )
     )
 }
--- Packages/TMDBKit/Tests/TMDBKitTests/TMDBKitTests.swift
+++ Packages/TMDBKit/Tests/TMDBKitTests/TMDBKitTests.swift
@@ -5,22 +5,6 @@ import Testing
 struct TMDBKitTests {
 
     @Test
-    func listMoviesUsesPopularMoviesEndpoint() throws {
-        let client = TMDBClient(
-            configuration: TMDBConfiguration(
-                authorization: .apiKey("demo-key"),
-                defaultLanguage: "en-US",
-                defaultRegion: "US"
-            )
-        )
-
-        let request = try client.makeRequest(for: .popularMovies(page: 2, language: "en-US", region: "US"))
-
-        #expect(request.url?.absoluteString == "https://api.themoviedb.org/3/movie/popular?language=en-US&page=2&region=US&api_key=demo-key")
-        #expect(request.httpMethod == "GET")
-    }
-
-    @Test
     func placeholderAuthorizationFailsEarly() {
         let client = TMDBClient(
             configuration: TMDBConfiguration(
